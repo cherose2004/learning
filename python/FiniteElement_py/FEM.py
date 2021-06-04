5fa331cc4c216c4f3dc9ebf8ce46edbe510f0c6b
bcynuaa <bcynuaa@163.com>
2021-06-05 02:53:07 +0800

    bcynuaa

--- python/FiniteElement_py/FEM.py
+++ python/FiniteElement_py/FEM.py
@@ -1,19 +1,21 @@
 import numpy as np
+NaN = np.NaN
 
 class Node:
 
-    def __init__(self , x = 0 , y = 0 , id = 0):
+    def __init__(self , x = 0 , y = 0 , Px = NaN , Py = NaN , u = NaN , v = NaN):
         self.x = x
         self.y = y
-        self.id = id
+        self.setP(Px , Py)
+        self.setUV(u , v)
         pass
 
-    def setF(self , Fx = 0, Fy = 0):
-        self.Fx = Fx
-        self.Fy = Fy
+    def setP(self , Px = NaN , Py = NaN):
+        self.Px = Px
+        self.Py = Py
         pass
 
-    def setUV(self , u = 0 , v = 0):
+    def setUV(self , u = NaN , v = NaN):
         self.u = u
         self.v = v
         pass
@@ -79,6 +81,84 @@ class SysPole:
                     pass
                 pass
             pass
+        print("successfully get matrix K")
+        pass
+
+    def setP(self , k , Px = NaN , Py = NaN):
+        self.NodeList[k-1].setP(Px , Py)
+        pass
+
+    def setUV(self , k , u = NaN , v = NaN):
+        self.NodeList[k-1].setUV(u , v)
+        pass
+
+    def getPart(self , x):
+        m = x.size
+        mat = np.zeros([m , m])
+        for j in range(m):
+            for i in range(m):
+                tmp = self.K[ int(x[j]) ][ int(x[i]) ]
+                mat[j][i] = tmp
+                pass
+            pass
+        return mat
+
+    def solve(self):
+        #提取借点已知信息
+        P = np.zeros([self.N , 2])
+        UV = np.zeros([self.N , 2])
+        for i in range(self.N):
+            node = self.NodeList[i]
+            P[i] = np.array([node.Px , node.Py])
+            UV[i] = np.array([node.u , node.v])
+            pass
+        #重塑为2*N矩阵
+        p = P.reshape(1 , 2*self.N)
+        uv = P.reshape(1 , 2*self.N)
+        #提取NaN位置信息，知道哪些未知量需要被处理
+        pNaN = list()
+        uvNaN = list()
+        for j in range(2*self.N):
+            if p[j] == NaN:
+                pNaN.append(j)
+                pass
+            if uv[j] == NaN:
+                uvNaN.append(j)
+                pass
+            pass
+        pNaN = np.array(pNaN)
+        uvNaN = np.array(uvNaN)
+        #处理前分块求解量
+        p1 = list()
+        p2 = list()
+        uv1 = list()
+        uv2 = list()
+        for j in range(2*self.N):
+            if j in pNaN:
+                p2.append(p[j])
+                pass
+            elif j not in pNaN:
+                p1.append(p[j])
+                pass
+            if j in uvNaN:
+                uv2.append(uv[j])
+                pass
+            elif j not in uvNaN:
+                uv1.append(uv[j])
+                pass
+            pass
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+        uv1 = np.array(uv1)
+        uv2 = np.array(uv2)
+        #矩阵分块
+        matp1 = self.getPart(uvNaN)
+        matp2 = self.getPart(pNaN)
+        matuv1 = self.getPart(pNaN)
+        matuv2 = self.getPart(uvNaN)
+        #开始求解
+
+
         pass
 
     pass
