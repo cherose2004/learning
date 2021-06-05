84af4917ddab81cda7fd7a1e8f5ba73077e8fcaf
bcynuaa <bcynuaa@163.com>
2021-06-05 14:05:06 +0800

    bcynuaa

--- python/FiniteElement_py/FEM.py
+++ python/FiniteElement_py/FEM.py
@@ -92,18 +92,31 @@ class SysPole:
         self.NodeList[k-1].setUV(u , v)
         pass
 
-    def getPart(self , x):
-        m = x.size
-        mat = np.zeros([m , m])
+    def getpartK(self , row , col):
+        m = row.size
+        n = col.size
+        mat = np.zeros([m , n])
         for j in range(m):
-            for i in range(m):
-                tmp = self.K[ int(x[j]) ][ int(x[i]) ]
-                mat[j][i] = tmp
+            for i in range(n):
+                mat[j][i] = self.K[ int(row[j]) ][ int(col[i]) ]
                 pass
             pass
         return mat
+        pass
+
+    def modifyK(self , pNaN , uvNaN):
+        n = pNaN.size
+        m = uvNaN.size
+        k11 = self.getpartK(uvNaN , pNaN)
+        k12 = self.getpartK(uvNaN , uvNaN)
+        k21 = self.getpartK(pNaN , pNaN)
+        k22 = self.getpartK(pNaN , uvNaN)
+        return k11 , k12 , k21 , k22
+        pass
+
 
     def solve(self):
+        
         #提取借点已知信息
         P = np.zeros([self.N , 2])
         UV = np.zeros([self.N , 2])
@@ -112,22 +125,24 @@ class SysPole:
             P[i] = np.array([node.Px , node.Py])
             UV[i] = np.array([node.u , node.v])
             pass
-        #重塑为2*N矩阵
-        p = P.reshape(1 , 2*self.N)
-        uv = P.reshape(1 , 2*self.N)
+        #重塑为1*2*N矩阵
+        p = P.flatten()
+        uv = UV.flatten()
+        
         #提取NaN位置信息，知道哪些未知量需要被处理
         pNaN = list()
         uvNaN = list()
         for j in range(2*self.N):
-            if p[j] == NaN:
+            if np.isnan(p[j]) == True:
                 pNaN.append(j)
                 pass
-            if uv[j] == NaN:
+            if np.isnan(uv[j]) == True:
                 uvNaN.append(j)
                 pass
             pass
-        pNaN = np.array(pNaN)
-        uvNaN = np.array(uvNaN)
+        pNaN = np.array(pNaN) #存储了力向量未知量的位置，也即位移向量已知量的位置
+        uvNaN = np.array(uvNaN) #存储了位移向量未知量的位置，也即力向量已知量的位置
+        
         #处理前分块求解量
         p1 = list()
         p2 = list()
@@ -147,18 +162,61 @@ class SysPole:
                 uv1.append(uv[j])
                 pass
             pass
-        p1 = np.array(p1)
-        p2 = np.array(p2)
-        uv1 = np.array(uv1)
-        uv2 = np.array(uv2)
+        p1 = np.array(p1) #存储力已知量列向量
+        p2 = np.array(p2) #存储力未知量列向量，NaN
+        uv1 = np.array(uv1) #存储位移已知量列向量
+        uv2 = np.array(uv2) #存储位移未知量列向量，NaN
+
         #矩阵分块
-        matp1 = self.getPart(uvNaN)
-        matp2 = self.getPart(pNaN)
-        matuv1 = self.getPart(pNaN)
-        matuv2 = self.getPart(uvNaN)
+        k11 , k12 , k21 , k22 = self.modifyK(pNaN , uvNaN)
+
         #开始求解
+        uv2 = np.linalg.inv(k12).dot(p1 - k11.dot(uv1))
+        p2 = k21.dot(uv1) + k22.dot(uv2)
+        t1 = 0
+        t2 = 0
+        for k in range(2*self.N):
+            if np.isnan(p[k]) == True:
+                p[k] = p2[t1]
+                t1 += 1
+                pass
+            if np.isnan(uv[k]) == True:
+                uv[k] = uv2[t2]
+                t2 += 1
+                pass
+            pass
+        #求解完毕，p与uv中已存储完毕整个力场与位移场
 
+        #将p于uv写入各个节点
+        for k in range(self.N):
+            self.NodeList[k].setP(Px = p[2*k] , Py = p[2*k+1])
+            self.NodeList[k].setUV(u = uv[2*k] , v = uv[2*k+1])
+            pass
+        print("successfully update node infomation")
+        pass
 
+    def info(self):
+        print('Report : Information of Each Node')
+        for i in range(20):
+            print('*' , end = '*')
+            pass
+        print('\n\n')
+        for k in range(self.N):
+            for i in range(10):
+                print('-' , end = '-')
+                pass
+            print('\n')
+            node = self.NodeList[k]
+            print('Node' + str(k+1) + ':\n')
+            print('Externel Load : ')
+            print('Horizontal Load Px = ' , node.Px)
+            print('Vertical Load Py = ' , node.Py)
+            print('\n')
+            print('Displacement : ')
+            print('Horizontal Displacement u = ' , node.u)
+            print('Vertical Displacement v = ' , node.v)
+            print('\n')
+            pass
         pass
 
     pass
